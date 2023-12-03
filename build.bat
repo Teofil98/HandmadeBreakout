4dea59110ff6e7e55bb2f7d5e1ab7323f74f11cf
Bodea Teofil <teo.bodea12@yahoo.com>
2023-12-03 19:58:43 +0100

    Add UNUSED macro

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build
 pushd build
-g++ -g ../win_hspaceinvaders.cpp -o win_hspaceinvaders.exe
+g++ -Wall -Wextra -Werror -g ../win_hspaceinvaders.cpp -o win_hspaceinvaders.exe
 popd
--- win_hspaceinvaders.cpp
+++ win_hspaceinvaders.cpp
@@ -1,14 +1,18 @@
 #include <windows.h>
-#include <winuser.h>
+#include "hspaceinvaders.h"
 
 int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR     lpCmdLine,
-  int       nShowCmd
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nShowCmd
 )
 {
+    UNUSED(hInstance);
+    UNUSED(hPrevInstance);
+    UNUSED(lpCmdLine);
+    UNUSED(nShowCmd);
     MessageBox(NULL, "Hello handmade Space invaders", "Handmade Space invaders",
-                MB_OK | MB_ICONINFORMATION);
+        MB_OK | MB_ICONINFORMATION);
     return 0;
 }

0fef67282526d9c45b5ee75dd6cde4175db6ff63
Bodea Teofil <teo.bodea12@yahoo.com>
2024-01-27 10:50:45 +0100

    Rename win_hspaceinvaders.cpp -> win32_hspaceinvaders.cpp

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build
 pushd build
-g++ -Wall -Wextra -Werror -pedantic -std=c++20 -g ../src/win_hspaceinvaders.cpp -o ../win_hspaceinvaders.exe -lgdi32 -lole32 -L "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" -l:xaudio2.lib
+g++ -Wall -Wextra -Werror -pedantic -std=c++20 -g ../src/win32_hspaceinvaders.cpp -o ../win32_hspaceinvaders.exe -lgdi32 -lole32 -L "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64" -l:xaudio2.lib
 popd

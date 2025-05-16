bff45130a2bf8d3fd44eb79b3e1288402a5c2584
Teofil Bodea <teo.bodea12@yahoo.com>
2025-05-16 21:47:01 +0200

    Add signof function to utils

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "defines.h"
+
 namespace my_lib {
 
 template <typename T>
@@ -12,6 +14,12 @@ template <typename T>
 inline T min(T a, T b)
 {
     return ((a < b) ? a : b);
+}
+
+template <typename T>
+inline int8 signof(T arg)
+{
+    return (arg < 0) ? -1 : 1;
 }
 
 } // namespace my_lib
